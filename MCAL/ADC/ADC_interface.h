17b819197e4e8aeffdfbfdb71b8d97ea913a6383
yosefarid <139150533+yosefarid@users.noreply.github.com>
2024-09-10 14:03:45 +0300

    ADC DRIVER

---
+++ MCAL/ADC/ADC_interface.h
@@ -0,0 +1,8 @@
+#ifndef ADC_INTERFACE_H
+#define ADC_INTERFACE_H
+
+void ADC_voidInit(void);
+
+uint16 ADC_u8GetChannelReading(uint8 Copy_u8Channel);
+
+#endif
---
+++ MCAL/ADC/ADC_register.h
@@ -0,0 +1,25 @@
+#ifndef ADC_REGISTER_H
+#define ADC_REGISTER_H
+
+#define ADMUX 				*((volatile uint8*)0x27)	/*ADC Multiplexer Selection Register*/
+#define ADMUX_REFS1			7u 							/*Reference selection BIT1*/
+#define ADMUX_REFS0			6u							/*Reference selection BIT0*/
+#define ADMUX_ADLAR			5u							/*Left Adjust Result*/
+
+
+#define ADCSRA				*((volatile uint8*)0x26)	/*ADC Control and Status Register A*/
+#define ADCSRA_ADEN			7u							/*ENABLE*/
+#define ADCSRA_ADSC			6u                          /*Start conversion*/
+#define ADCSRA_ADATE		5u                          /*Auto trigger enable*/
+#define ADCSRA_ADIF			4u                          /*Interrupt flag*/
+#define ADCSRA_ADIE			3u                          /*interrupt ENABLE*/
+
+#define SFIOR				*((volatile uint8*)0x50)	/*Special FunctionIO Register*/
+
+#define ADCH				*((volatile uint8*)0x25)	/*ADC Data High Register*/
+#define ADCL				*((volatile uint8*)0x24)    /*ADC data low Register*/
+#define ADC					*((volatile uint16*)0x24)	/*10 Bit Resolution*/
+
+
+
+#endif
