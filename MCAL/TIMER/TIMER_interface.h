9834d7b26ac5e7a24854125c67457bc575892c5f
yosefarid <139150533+yosefarid@users.noreply.github.com>
2024-09-22 23:41:50 +0300

    Timer_1.1

--- MCAL/TIMER/TIMER_interface.h
+++ MCAL/TIMER/TIMER_interface.h
@@ -1,25 +1,17 @@
 #ifndef TIMER_INTERFACE_H
 #define TIMER_INTERFACE_H
 
-#include "TIMER_register.h"
-
 #define TIMER0_OVF_INT			0u
 #define TIMER0_OC_INT			1u
 #define TIMER1_OVF_INT			2u
 #define TIMER1_B_OC_INT			3u
 #define TIMER1_A_OC_INT			4u
-#define TIMER0_ICU_INT			5u
+#define TIMER1_ICU_INT			5u
 #define TIMER2_OVF_INT			6u
 #define TIMER2_OC_INT			7u
 
-#define TIMER0_Preload			&TCNT0
-#define TIMER1_Preload			&TCNT1
-#define TIMER2_Preload			&TCNT2
-
-#define TIMER0_Comp_Val			&OCR0
-#define TIMER1A_Comp_Val		&OCR1A
-#define TIMER1B_Comp_Val		&OCR1B
-#define TIMER2_Comp_Val			&OCR2
+#define	Falling_edge			0u
+#define rising_edge				1u
 
 typedef enum
 {
@@ -54,12 +46,24 @@ typedef enum
 	EXT_Clock_Rising_Edge
 }Prescaler_t;
 
+typedef enum
+{
+	NO_DIV_2 = 1,
+	DIV_BY_8_2,
+	DIV_BY_32,
+	DIV_BY_64_2,
+	DIV_BY_128,
+	DIV_BY_256_2,
+	DIV_BY_1024_2
+}Prescale_2_t;
+
 typedef struct
 {
 	WGM_t Wave_mode;
 	COM_CTC_t Compare_Mode;
 	COM_PWM_t Compare_PWM_Mode;
 	Prescaler_t Prescaler;
+	Prescale_2_t Prescaler_2;
 }TIM_0_2_t;
 
 typedef enum
@@ -96,9 +100,6 @@ typedef struct
 	Prescaler_t Prescaler;
 }TIM_1_t;
 
-void TIMER_void_Int_Enable(uint8 Copy_u8_INT);
-
-void TIMER_void_Int_Disable(uint8 Copy_u8_INT);
 
 uint8 TIMER0_u8Init(TIM_0_2_t* Copy_Configuration);
 
@@ -106,12 +107,30 @@ uint8 TIMER1_u8Init(TIM_1_t* Copy_Configuration);
 
 uint8 TIMER2_u8Init(TIM_0_2_t* Copy_Configuration);
 
-uint8 TIMER_u8SetCallBack(uint8 Copy_uu8IntSource , void(*Copy_pvCallBackFunc)(void));
+void TIMER0_voidSetPreloadValue(uint8 Copy_u16PreloadVal);
 
-void TIMER_voidSetPreloadValue(uint16 Copy_u16PreloadVal , volatile  uint8* Copy_pu8TIMER);
+void TIMER1_voidSetPreloadValue(uint16 Copy_u16PreloadVal);
 
-void TIMER_voidSetCompareMatchValue(uint16 Copy_u16CompMatchVal ,volatile  uint8* Copy_pu8TIMER);
+void TIMER2_voidSetPreloadValue(uint8 Copy_u16PreloadVal);
+
+void TIMER0_voidSetCompareMatchValue(uint8 Copy_u16CompMatchVal);
+
+void TIMER1A_voidSetCompareMatchValue(uint16 Copy_u16CompMatchVal);
+
+void TIMER1B_voidSetCompareMatchValue(uint16 Copy_u16CompMatchVal);
+
+void TIMER2_voidSetCompareMatchValue(uint8 Copy_u16CompMatchVal);
+
+void TIMER_void_Int_Enable(uint8 Copy_u8_INT);
+
+void TIMER_void_Int_Disable(uint8 Copy_u8_INT);
+
+uint8 TIMER_u8SetCallBack(uint8 Copy_u8IntSource , void(*Copy_pvCallBackFunc)(void));
 
 void TIMER_voidSetInputCaptureValue(uint16 Copy_u8CompMatchVal);
+
+void TIMER_voidChooseICUTrigger(uint8 Copy_u8Trigger);
+
+uint16 TIMER_u16GetICUValue(void);
 
 #endif
